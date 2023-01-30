e47655669c940639b0cecf1543ebf995971a0967
hagen <hagen.geissler@funkemedien.de>
2023-01-31 00:28:27 +0100

    adding source location urls for sat sdo tool 1600

--- bin/loop-dates.sh
+++ bin/loop-dates.sh
@@ -1,13 +1,20 @@
 
 
-for sat in 1600 1700; do
-for y in {2020..2022}; do
+for sat in 1700 1600; do
+for y in {2010..2022}; do
 for m in 01 02 03 04 05 06 07 08 09 10 11 12; do
 outfile="../lists/$sat-$y-$m.txt"
-
+minimumsize=100
+actualsize=$(du -k "$outfile" | cut -f 1)
 
 if [ ! -f "$outfile" ]; then
   echo " bash sdo-get.sh $sat $y $m" | sh -x > $outfile;
+else
+  if [ $actualsize -ge $minimumsize ]; then
+      echo size is over $minimumsize kilobytes
+  else
+      echo " bash sdo-get.sh $sat $y $m" | sh -x > $outfile;
+  fi
 fi
 done;
 done;
