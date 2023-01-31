150b50e82334c968a11826f990230e519fd3c680
hagen <hagen.geissler@funkemedien.de>
2023-01-31 20:22:56 +0100

    full source image locations fore sdo 1600 & 1700 from 2010 2023

--- bin/loop-dates.sh
+++ bin/loop-dates.sh
@@ -1,10 +1,10 @@
 
 
 for sat in 1700 1600; do
-for y in {2011..2022}; do
+for y in {2010..2022}; do
 for m in 01 02 03 04 05 06 07 08 09 10 11 12; do
 outfile="../lists/$sat-$y-$m.txt"
-minimumsize=100
+minimumsize=50
 actualsize=$(du -k "$outfile" | cut -f 1)
 
 if [ ! -f "$outfile" ]; then
