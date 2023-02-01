90d726bd128420017dba86e53acbf8b78a3cf872
hagen <hagen.geissler@funkemedien.de>
2023-02-01 13:03:54 +0100

    adding current source locations for soho sat system c2 & c3

--- bin/soho-get.sh
+++ bin/soho-get.sh
@@ -1,13 +1,13 @@
 
-start=$(date +"%Y%m%d")
+start=$2
 sat=$1
-end=$2
-while ! [[ $start < $end ]]; do
-    #echo $start
-
-for i in $( mojo get "https://soho.nascom.nasa.gov/data/REPROCESSING/Completed/2022/$sat/$start/" a attr href | egrep "1024");
+end=$(date +"%Y%m%d")
+year=${start:0:4}
+while ! [[ $start > $end ]]; do
+ 
+for i in $( mojo get "https://soho.nascom.nasa.gov/data/REPROCESSING/Completed/$year/$sat/$start/" a attr href | egrep "1024");
 do
-echo "https://soho.nascom.nasa.gov/data/REPROCESSING/Completed/2022/$sat/$start/$i";
+echo "https://soho.nascom.nasa.gov/data/REPROCESSING/Completed/$year/$sat/$start/$i";
 done
-start=$(date -j -v-1d -f %Y%m%d $start +%Y%m%d)
+start=$(date -j -v+1d -f %Y%m%d $start +%Y%m%d)
 done
